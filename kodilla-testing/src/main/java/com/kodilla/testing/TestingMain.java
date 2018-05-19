161746bb0ad37d58f4f4af59bbf674b42522770c
fettpl <pawel.borawski@gmail.com>
2018-05-19 20:01:55 +0200

    Calculator test added.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+import java.util.Objects;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("User test: OK");
+        } else {
+            System.out.println("User test: Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        Integer addResult = calculator.add(4, 2);
+        Integer substractResult = calculator.subtrackt(4, 2);
+
+        if (addResult == 6) {
+            System.out.println("Calculator add test: OK");
+        } else {
+            System.out.println("Calculator add test: Error!");
+        }
+
+        if (substractResult == 2) {
+            System.out.println("Calculator substract test: OK");
+        } else {
+            System.out.println("Calculator substract test: Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int A;
+    private int B;
+
+    public int getA() {
+        return A;
+    }
+
+    public int getB() {
+        return B;
+    }
+
+    public int add(int A, int B) {
+        return A + B;
+    }
+
+    public int subtrackt(int A, int B) {
+        return A - B;
+    }
+}
