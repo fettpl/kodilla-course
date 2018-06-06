76e42b21b84fad606fcccc7871855919f54fb895
fettpl <pawel.borawski@gmail.com>
2018-06-06 19:13:39 +0200

    Updated PoemBeautifier.

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -1,7 +1,5 @@
 package com.kodilla.stream.beautifier;
 
-import static com.kodilla.stream.beautifier.PoemDecorator.*;
-
 public class PoemBeautifier {
     public void beautify(String text, String decorator, PoemDecorator poemDecorator) {
         String result = poemDecorator.decorate(text, decorator);
