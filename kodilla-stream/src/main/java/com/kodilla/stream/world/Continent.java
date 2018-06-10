22f24d709e20829b5934193514a40735541e9554
fettpl <pawel.borawski@gmail.com>
2018-06-10 19:26:16 +0200

    Feat (Added "sand" packaged with Test Suite) Feat (Added "simple invoice" package with Test Suite) Feat (Added Forum User with Test Suite) Feat (Added "world" package) Update (JUnit dependency added to build.gradle)

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -0,0 +1,9 @@
+package com.kodilla.stream.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Continent {
+    List<Country> europe = new ArrayList<>();
+    europe.add(new Country().BigDecimal("38000000"));
+}
