a1acdd130f0dc7986e2c1260a1298b97e7b9245f
fettpl <pawel.borawski@gmail.com>
2018-06-16 15:22:34 +0200

    Feat (Added ArrayOperations Interfase and matching test Suite)

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -5,7 +5,6 @@ import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static int getAverage(int[] numbers) {
-        List<int> numbers = new ArrayList<>();
         List<int> numbersList = IntStream.range(0, ((ArrayList) numbers).getList());
 
         double median = (double) IntStream.range(0, ((ArrayList) numbers).average());
