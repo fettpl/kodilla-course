af626b5e3ea73e685d1c7f95e14b7e2c8e0c906a
fettpl <pawel.borawski@gmail.com>
2018-06-30 18:42:31 +0200

    Fix (Updated StatsAnalytics); Feat (Added a new set of Tests for Statistics).

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -1,11 +1,14 @@
 package com.kodilla.testing.forum.statistics;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
 
+import static java.lang.Double.NaN;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -15,34 +18,69 @@ public class StatisticsTestSuite {
     public void calculateAdvStatistics() {
 
         // Given
-        StatsAnalytics statsAnalyticsMock  = mock(StatsAnalytics.class);
-
-        List<String> usersNames = new ArrayList<>()List<String>() {
-            usersNames.add("User1");
-            usersNames.add("User2");
-            usersNames.add("User3");
-            usersNames.add("User4");
-            usersNames.add("User5");
-        }
-        List<String> postsList = new ArrayList<>()List<String>() {
-            postsList.add("Lorem ipsum dolor 1");
-            postsList.add("Lorem ipsum dolor 2");
-            postsList.add("Lorem ipsum dolor 3");
-            postsList.add("Lorem ipsum dolor 4");
-            postsList.add("Lorem ipsum dolor 5");
-        }
-        List<String> commentsList = new ArrayList<>()List<String>() {
-            commentsList.add("Sit amet, consectetur adipiscing elit 1");
-            commentsList.add("Sit amet, consectetur adipiscing elit 2");
-            commentsList.add("Sit amet, consectetur adipiscing elit 3");
-            commentsList.add("Sit amet, consectetur adipiscing elit 4");
-            commentsList.add("Sit amet, consectetur adipiscing elit 5");
-        }
+        Statistics statistics = mock(Statistics.class);
+        StatsAnalytics statsAnalytics = new StatsAnalytics();
+        List<String> listOfUsers = new ArrayList();
+        listOfUsers.add("AAA");
+        listOfUsers.add("BBB");
+        listOfUsers.add("CCC");
+        when(statistics.usersNames()).thenReturn(listOfUsers);
+        when(statistics.postsCount()).thenReturn(30);
+        when(statistics.commentsCount()).thenReturn(40);
 
         // When
-        when(statsAnalyticsMock.calculateAdvStatistic()).then()
+        statsAnalytics.calculateAdvStatistics(statistics);
 
         // Then
+        assertEquals(3, statsAnalytics.getNumberOfUsers());
+        assertEquals(30, statsAnalytics.getNumberOfPosts());
+        assertEquals(40, statsAnalytics.getNumberOfComments());
+        assertEquals(1.3, statsAnalytics.getAverageCommentsPerPost(), 0.1);
+        assertEquals(13.3, statsAnalytics.getAverageCommentsPerUser(), 0.1);
+        assertEquals(10.0, statsAnalytics.getAveragePostsPerUser(), 0.1);
+    }
 
+    @Test
+    public void calculateAdvStatisticsWithZeros() {
+
+        // Given
+        Statistics statistics = mock(Statistics.class);
+        StatsAnalytics statsAnalytics = new StatsAnalytics();
+        List<String> listOfUsers = new ArrayList();
+        listOfUsers.add("DDD");
+        listOfUsers.add("EEE");
+        listOfUsers.add("FFF");
+        when(statistics.usersNames()).thenReturn(listOfUsers);
+        when(statistics.postsCount()).thenReturn(0);
+        when(statistics.commentsCount()).thenReturn(0);
+
+        // When
+        statsAnalytics.calculateAdvStatistics(statistics);
+
+        // Then
+        assertEquals(3, statsAnalytics.getNumberOfUsers());
+        assertEquals(0, statsAnalytics.getNumberOfPosts());
+        assertEquals(0, statsAnalytics.getNumberOfComments());
+        assertEquals(Optional.ofNullable(NaN), java.util.Optional.ofNullable(statsAnalytics.getAverageCommentsPerPost()));
+        assertEquals(Optional.of(0.0), java.util.Optional.ofNullable(statsAnalytics.getAverageCommentsPerUser()));
+        assertEquals(Optional.of(0.0), java.util.Optional.ofNullable(statsAnalytics.getAveragePostsPerUser()));
+    }
+
+    @Test
+    public void calculateAdvStatisticsZeroUsers() {
+
+        // Given
+        Statistics statistics = mock(Statistics.class);
+        StatsAnalytics statsAnalytics = new StatsAnalytics();
+        List<String> listOfUsers = new ArrayList();
+        when(statistics.usersNames()).thenReturn(listOfUsers);
+
+        // When
+        statsAnalytics.calculateAdvStatistics(statistics);
+
+        // Then
+        assertEquals(0, statsAnalytics.getNumberOfUsers());
+        assertEquals(0, statsAnalytics.getNumberOfPosts());
+        assertEquals(0, statsAnalytics.getNumberOfComments());
     }
 }
