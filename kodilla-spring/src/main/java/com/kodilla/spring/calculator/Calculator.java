6354e8b994d207c46700aa2bd0a073f1d1f60864
fettpl <pawel.borawski@gmail.com>
2018-07-23 15:29:07 +0200

    Feat (Added Calculator, Libray, Shape packages); Feat (Added SpringRunnerTestSuite); Feat (Added LibraryTestSuite).

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+    private Display display;
+
+    @Autowired
+    private Display useDisplay;
+}
