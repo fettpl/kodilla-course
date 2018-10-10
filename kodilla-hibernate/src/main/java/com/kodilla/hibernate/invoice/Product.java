53181f420d42258116aeef844c889b7c74266e9c
fettpl <pawel.borawski@gmail.comgit config --global user.name fettpl>
2018-10-10 16:02:58 +0200

    Fix: InvoiceDaoTestSuite updates

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Product.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Product.java
@@ -2,6 +2,8 @@ package com.kodilla.hibernate.invoice;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "PRODUCTS")
@@ -10,6 +12,7 @@ public class Product {
     @GeneratedValue
     private int id;
     private String name;
+    private List<Item> items = new ArrayList<>();
 
     public Product() {
 
@@ -38,5 +41,13 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
     }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -2,6 +2,7 @@ package com.kodilla.hibernate.invoice.dao;
 
 import com.kodilla.hibernate.invoice.Invoice;
 import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,19 +28,51 @@ public class InvoiceDaoTestSuite {
     @Test
     public void testInvoiceDaoSave() {
         //Given
-        Invoice invoice = new Invoice("001/2018");
-        invoice.getItems().add(new Item(new BigDecimal(100.99), 5, new BigDecimal(59.99)));
-        invoice.getItems().add(new Item(new BigDecimal(22.49), 2, new BigDecimal(79.29)));
-        invoice.getItems().add(new Item(new BigDecimal(40.99), 11, new BigDecimal(9.99)));
+        Product product1 = new Product("Product 1");
+        Product product2 = new Product("Product 2");
+        Product product3 = new Product("Product 3");
+        productDao.save(product1);
+        productDao.save(product2);
+        productDao.save(product3);
+        Item item1 = new Item(new BigDecimal(100), 3, new BigDecimal(100));
+        Item item2 = new Item(new BigDecimal(250), 2, new BigDecimal(100));
+        Item item3 = new Item(new BigDecimal(10), 7, new BigDecimal(100));
+        Item item4 = new Item(new BigDecimal(100), 2, new BigDecimal(100));
+
+        item1.setProduct(product1);
+        item2.setProduct(product2);
+        item3.setProduct(product3);
+        item4.setProduct(product1);
+        product1.getItems().add(item1);
+        product1.getItems().add(item4);
+        product2.getItems().add(item2);
+        product3.getItems().add(item3);
+
+
+        Invoice invoice1 = new Invoice("FV_NO1");
+        Invoice invoice2 = new Invoice("FV_NO2");
+        invoice1.getItems().add(item1);
+        invoice1.getItems().add(item2);
+        invoice1.getItems().add(item3);
+        invoice2.getItems().add(item4);
+
+        item1.setInvoice(invoice1);
+        item2.setInvoice(invoice1);
+        item3.setInvoice(invoice1);
+        item4.setInvoice(invoice2);
 
         //When
-        invoiceDao.save(invoice);
+        invoiceDao.save(invoice1);
+        invoiceDao.save(invoice2);
 
         //Then
-        int createdInvoiceId = invoice.getId();
-        Assert.assertNotEquals(0, createdInvoiceId);
+        int createdInvoiceId1 = invoice1.getId();
+        Assert.assertNotEquals(0, createdInvoiceId1);
+        int createdInvoiceId2 = invoice2.getId();
+        Assert.assertNotEquals(0, createdInvoiceId2);
 
         //CleanUp
-        invoiceDao.delete(invoice);
+        invoiceDao.delete(invoice1);
+        invoiceDao.delete(invoice2);
     }
 }
