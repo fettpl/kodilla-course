06a5eed6cdea200cf53d7e9e5a3e5f5837c4f06e
fettpl <pawel.borawski@gmail.comgit config --global user.name fettpl>
2018-10-11 13:55:55 +0200

    Feat: Added ProductProcessor with Shop classes

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/food2door/ExtraFoodShop.java
@@ -0,0 +1,10 @@
+package com.kodilla.patterns.food2door;
+
+public class ExtraFoodShop implements ProductProcessor {
+
+    @Override
+    public boolean process() {
+        System.out.println("Lorem ipsum dolor");
+        return true;
+    }
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/food2door/ProductProcessor.java
@@ -0,0 +1,6 @@
+package com.kodilla.patterns.food2door;
+
+public interface ProductProcessor {
+
+    boolean process();
+}
